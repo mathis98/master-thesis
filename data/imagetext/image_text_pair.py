11b82021bf091e7d2f9840b91910b38adaa0cd3c
hacktron <mathis.arend@gmail.com>
2023-11-07 09:16:46 +0100

    Fix ImageText DataModule return batches after first epoch

--- data/imagetext/image_text_pair.py
+++ data/imagetext/image_text_pair.py
@@ -1,10 +1,11 @@
 import pytorch_lightning as pl
 
-class ImageTextPairDataLoader(pl.LightningDataModule):
-	def __init__(self, image_dataloader, text_dataloader):
-		super(ImageTextPairDataLoader, self).__init__()
-		self.image_dataloader = image_dataloader
-		self.text_dataloader = text_dataloader
+class ImageTextPairDataModule(pl.LightningDataModule):
+	def __init__(self, image_data_module, text_data_module, batch_size=64):
+		super(ImageTextPairDataModule, self).__init__()
+		self.image_data_module = image_data_module
+		self.text_data_module = text_data_module
+		self.batch_size = batch_size
 
 	def prepare_data(self):
 		pass
@@ -13,23 +14,23 @@ class ImageTextPairDataLoader(pl.LightningDataModule):
 		pass
 
 	def train_dataloader(self):
-		for image_batch, text_batch in zip(
-			self.image_dataloader.train_dataloader(), 
-			self.text_dataloader.train_dataloader()
-		):
-			yield image_batch, text_batch
+		for _ in range(len(self.image_data_module.train_dataloader())):
+			image_dataloader = list(self.image_data_module.train_dataloader())
+			text_dataloader = list(self.text_data_module.train_dataloader())
+			for image_batch, text_batch in zip(image_dataloader, text_dataloader):
+				yield image_batch, text_batch
 
 	def val_dataloader(self):
-		for image_batch, text_batch in zip(
-			self.image_dataloader.val_dataloader(), 
-			self.text_dataloader.val_dataloader()
-		):
-			yield image_batch, text_batch
+		for _ in range(len(self.image_data_module.val_dataloader())):
+			image_dataloader = list(self.image_data_module.val_dataloader())
+			text_dataloader = list(self.text_data_module.val_dataloader())
+			for image_batch, text_batch in zip(image_dataloader, text_dataloader):
+				yield image_batch, text_batch
 
 
 	def test_dataloader(self):
-		for image_batch, text_batch in zip(
-			self.image_dataloader.test_dataloader(), 
-			self.text_dataloader.test_dataloader()
-		):
-			yield image_batch, text_batch
+		for _ in range(len(self.image_data_module.test_dataloader())):
+			image_dataloader = list(self.image_data_module.test_dataloader())
+			text_dataloader = list(self.text_data_module.test_dataloader())
+			for image_batch, text_batch in zip(image_dataloader, text_dataloader):
+				yield image_batch, text_batch
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from model.full_pipeline import FullPipeline
 
 from data.image.data_module import ImageDataModule
 from data.text.data_module import SentenceDataModule
-from data.imagetext.image_text_pair import ImageTextPairDataLoader
+from data.imagetext.image_text_pair import ImageTextPairDataModule
 
 text_path = '../Datasets/UCM/dataset.json'
 img_path = '../Datasets/UCM/imgs'
@@ -24,7 +24,7 @@ text_data_module = SentenceDataModule(model_name, batch_size, text_path)
 text_data_module.prepare_data()
 text_data_module.setup(stage='fit')
 
-image_text_pair_dataloader = ImageTextPairDataLoader(image_data_module, text_data_module)
+image_text_pair_data_module = ImageTextPairDataModule(image_data_module, text_data_module, batch_size)
 
 
 # print('image: ')
@@ -32,7 +32,7 @@ image_text_pair_dataloader = ImageTextPairDataLoader(image_data_module, text_dat
 # print('text: ')
 # print(text_data_module.train_dataset.sentences[:10])
 # print('both: ')
-# elem = next(image_text_pair_dataloader.train_dataloader())
+# elem = next(image_text_pair_data_module.train_dataloader())
 # print(elem[0][1][:10], elem[1][1][:10])
 
 
@@ -42,9 +42,9 @@ trainer = pl.Trainer()
 
 trainer.fit(
 	full_pipeline, 
-	image_text_pair_dataloader.train_dataloader(),
-	image_text_pair_dataloader.val_dataloader(),
+	image_text_pair_data_module.train_dataloader(),
+	image_text_pair_data_module.val_dataloader(),
 )
 
-trainer.test(full_pipeline, dataloaders=image_text_pair_dataloader.test_dataloader())
+trainer.test(full_pipeline, dataloaders=image_text_pair_data_module.test_dataloader())
 
