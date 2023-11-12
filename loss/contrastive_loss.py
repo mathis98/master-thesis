5cf7a778475f480ec7d6da10b2272c5de3c3d5cb
hacktron <mathis.arend@gmail.com>
2023-11-12 13:44:08 +0100

    Fix simclr

--- loss/contrastive_loss.py
+++ loss/contrastive_loss.py
@@ -20,21 +20,23 @@ class SimCLRLoss(nn.Module):
 		return F.cosine_similarity(representations.unsqueeze(1), representations.unsqueeze(0), dim=2)
 
 	def forward(self, z_i, z_j):
-		z_i = F.normalize(z_i, dim=1, p=2)
-		z_j = F.normalize(z_j, dim=1, p=2)
+		batch_size = z_i.shape[0]
 		
+		z_i = F.normalize(z_i, p=2, dim=1)
+		z_j = F.normalize(z_j, p=2, dim=1)
+
 		similarity_matrix = self.calc_similarity_batch(z_i, z_j)
 
-		sim_ij = torch.diag(similarity_matrix, int(self.batch_size))
-		sim_ji = torch.diag(similarity_matrix, -int(self.batch_size))
+		sim_ij = torch.diag(similarity_matrix, batch_size)
+		sim_ji = torch.diag(similarity_matrix, -batch_size)
 
 		positives = torch.cat([sim_ij, sim_ji], dim=0)
 
 		nominator = torch.exp(positives / self.temperature)
+
 		denominator = device_as(self.mask, similarity_matrix) * torch.exp(similarity_matrix / self.temperature)
 
 		all_losses = -torch.log(nominator / torch.sum(denominator, dim=1))
-
-		loss = torch.sum(all_losses) / (2 * int(self.batch_size))
-
+		loss = torch.sum(all_losses) / (2 * self.batch_size)
+		
 		return loss
