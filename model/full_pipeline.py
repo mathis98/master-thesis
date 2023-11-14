564b6926b32c7836b5d387bceaf8bef745e02dd9
hacktron <mathis.arend@gmail.com>
2023-11-14 17:22:59 +0100

    fix debug2

--- model/full_pipeline.py
+++ model/full_pipeline.py
@@ -143,8 +143,8 @@ class FullPipeline(pl.LightningModule):
 		# image: (original_image, image_path)
 		# caption: (inputs, sentence, index)
 
-		print(image[0])
-		print(caption[0])
+		print(image)
+		print(caption)
 
 		indeces = caption[2]
 		labels = indeces // 100
