68d5783bcaa76e2fa861ee0a946235fe794850f5
hacktron <mathis.arend@gmail.com>
2023-11-26 15:01:51 +0100

    fix initialization of projection head

--- model/projection_head.py
+++ model/projection_head.py
@@ -4,11 +4,11 @@ class MyProjectionhead(nn.Module):
 	def __init__(self, input_dim, hidden_dim, output_dim):
 		super(MyProjectionhead, self).__init__()
 
-	self.linear1 = nn.Linear(input_dim, hidden_dim)
-	self.bn1 = nn.BatchNorm1d(hidden_dim)
-	self.relu = nn.ReLU()
-	self.linear2 = nn.linear(hidden_dim, output_dim)
-	self.bn2 = nn.BatchNorm1d(output_dim)
+		self.linear1 = nn.Linear(input_dim, hidden_dim)
+		self.bn1 = nn.BatchNorm1d(hidden_dim)
+		self.relu = nn.ReLU()
+		self.linear2 = nn.linear(hidden_dim, output_dim)
+		self.bn2 = nn.BatchNorm1d(output_dim)
 
 	def forward(self, x):
 		x = self.linear1(x)
