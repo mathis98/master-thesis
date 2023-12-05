bcc0f68c3e65b696be8c3273b57f960012d6bd89
hacktron <mathis.arend@gmail.com>
2023-12-06 00:05:10 +0100

    hotfix 2

--- utility/helpers.py
+++ utility/helpers.py
@@ -142,7 +142,7 @@ def calculate_mAP(image_embeddings, caption_embeddings, ground_truth_labels, top
 		else:
 			# Convert to numpy for indexing
 			ranked = ranked_indices.cpu().numpy()[:top_k]
-			precision = np.cumsum(relevant_labels[ranked].cpu().numpy()) / (np.arange(1, top_k+1))
+			precision = np.cumsum(relevant_labels.cpu().numpy()[ranked]) / (np.arange(1, top_k+1))
 			AP = np.sum(precision * relevant_labels[ranked]) / num_relevant_images
 
 		mAP_values.append(AP)
