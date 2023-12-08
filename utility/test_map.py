5365aa257894a676581da2ec3f186bf907c85c69
hacktron <mathis.arend@gmail.com>
2023-12-08 12:28:57 +0100

    update checkpointint

--- utility/test_map.py
+++ utility/test_map.py
@@ -25,10 +25,7 @@ batch_size = 32
 
 
 # Create an instance of your FullPipeline model
-model = FullPipeline.load_from_checkpoint('../logs/full_pipeline_full_val_test/version_193/checkpoints/epoch=7-avg_val_mAP=0.36-validation mAP=0.38.ckpt')
-
-# Ensure the model is in evaluation mode
-model.test()
+checkpoint = '../logs/full_pipeline_full_val_test/version_193/checkpoints/epoch=7-avg_val_mAP=0.36-validation mAP=0.38.ckpt'
 
 
 image_data_module = ImageDataModule('../../Datasets/UCM/imgs', (224,224), batch_size, 5)
@@ -51,4 +48,4 @@ print(f'training on GPU {devices}')
 trainer = pl.Trainer(accelerator='cuda', devices=[1], max_epochs=100)
 
 with torch.no_grad():
-		trainer.test(model, dataloaders=image_text_pair_data_module.test_dataloader())
+		trainer.test(ckpt_path=checkpoint, dataloaders=image_text_pair_data_module.test_dataloader())
