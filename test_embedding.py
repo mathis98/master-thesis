20bd331333ebb2f5a9ad60caa37bdadd7706eadf
hacktron <mathis.arend@gmail.com>
2023-12-09 17:24:10 +0100

    debug output

--- test_embedding.py
+++ test_embedding.py
@@ -80,6 +80,8 @@ while True:
 
 	new_caption = tokenizer(new_caption)
 
+	print(image_text_pair_data_module.dataset[0])
+
 	new_caption_embedding = full_pipeline.bert_embedding_module(torch.tensor(new_caption))
 	new_caption_projection = full_pipeline.projection_head(new_caption_embedding)
 
