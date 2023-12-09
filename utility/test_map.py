d58449e5fd7af2b3a80827ec78fe1c1b6d207e24
hacktron <mathis.arend@gmail.com>
2023-12-09 10:23:20 +0100

    change test map batch size

--- utility/test_map.py
+++ utility/test_map.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 from utility.argument_parser import parse_arguments
 from utility.helpers import to_cuda_recursive
 
-batch_size = 32
+batch_size = 64
 
 
 image_data_module = ImageDataModule('../../Datasets/UCM/imgs', (224,224), batch_size, 5)
