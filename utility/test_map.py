90d36e6223539b8ffa58ad24bdd6b7ed15a47a4f
hacktron <mathis.arend@gmail.com>
2023-12-09 11:59:30 +0100

    at different batch size

--- utility/test_map.py
+++ utility/test_map.py
@@ -29,7 +29,7 @@ import matplotlib.pyplot as plt
 from utility.argument_parser import parse_arguments
 from utility.helpers import to_cuda_recursive
 
-batch_size = 64
+batch_size = 8
 
 intra = False
 
