647da936c79e940ed2ffc8817731aa2d4d516ac9
hacktron <mathis.arend@gmail.com>
2023-12-19 10:10:45 +0100

    new print

--- data/text/data_module.py
+++ data/text/data_module.py
@@ -79,7 +79,7 @@ class SentenceDataModule(pl.LightningDataModule):
 		if self.technique == 'Concat':
 			sentences = [' '.join([item['sentences'][i]['raw'] for i in range(5)]) for item in data['images']]
 
-		print(sentences[:3])
+		print(sentences[:5])
 
 		total_size = len(sentences)
 		train_size = int(.8 * total_size)
