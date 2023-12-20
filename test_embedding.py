9c60bce0595ab493bae5714d3ce89b990ed15571
hacktron <mathis.arend@gmail.com>
2023-12-20 10:52:21 +0100

    list all text

--- test_embedding.py
+++ test_embedding.py
@@ -98,10 +98,10 @@ full_pipeline.eval()
 image_embeddings, labels = full_pipeline.calculate_embeddings_for_images(validation=False, true_label=True)
 
 
-# print('text:')
-# print(list(text_data_module.test_dataset)[:5])
-print('image:')
-print(list(image_data_module.test_dataset))
+print('text:')
+print(list(text_data_module.test_dataset))
+# print('image:')
+# print(list(image_data_module.test_dataset))
 # print('imagetext:')
 # print(list(image_text_pair_data_module.test_dataset)[:5])
 
