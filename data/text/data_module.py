d2540b58b54c64f170a86d0842be9bf4678a3030
Mathis Arend <mathisarend@Mathiss-MBP.fritz.box>
2023-12-28 07:19:28 -0800

    fix error assignment

--- data/text/data_module.py
+++ data/text/data_module.py
@@ -74,6 +74,8 @@ class SentenceDataModule(pl.LightningDataModule):
 		with open(self.json_file_path, 'r') as json_file:
 			data = json.load(json_file)
 
+		sentences = []
+
 		if self.technique == 'Concat':
 
 			if 'NWPU' in self.json_file_path:
