1815ad681f81f77b35a89a2a05b994ef9ff29094
Mathis Arend <mathisarend@Mathiss-MBP.fritz.box>
2023-12-28 06:59:53 -0800

    start random

--- data/text/data_module.py
+++ data/text/data_module.py
@@ -58,7 +58,7 @@ class SentenceDataModule(pl.LightningDataModule):
 		json_file_path (str): path to the json file containing the captions
 		seed (int): seed for shuffling
 	"""
-	def __init__(self, model_name, batch_size, json_file_path, seed=42, num_repeats=5, technique='Concat', rand=3):
+	def __init__(self, model_name, batch_size, json_file_path, seed=42, num_repeats=5, technique='Random', rand=3):
 		super(SentenceDataModule, self).__init__()
 		self.model_name = model_name
 		self.batch_size = batch_size
@@ -89,7 +89,13 @@ class SentenceDataModule(pl.LightningDataModule):
 		elif self.technique == 'Random':
 
 			if 'NWPU' in self.json_file_path:
-				pass
+				sentences = []
+				categories = sorted([category for category in data])
+
+				for category in categories:
+					key = 'raw' if self.rand == 0 else f'raw_{self.rand}'
+					sentences.extend([item[key]] for item in data[category])
+				print(sentences[:5])
 			else:
 				sentences = [[item['sentences'][i]['raw'] for i in range(5)][self.rand] for item in data['images']]
 
