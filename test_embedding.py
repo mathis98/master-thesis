209d9a0b42dad08cf33014486340965c69e3f07a
Mathis Arend <mathisarend@Mathiss-MacBook-Pro.local>
2024-01-10 15:53:01 +0800

    fix indexing for test embeddings

--- test_embedding.py
+++ test_embedding.py
@@ -137,7 +137,7 @@ random_sample = random.sample(list(text_data_module.test_dataset), 5)
 
 print('5 Random samples:')
 for element in random_sample:
-	print(f'Sentence: {element[1]} (Index: {element[2] // num_repeats + 1})')
+	print(f'Sentence: {element[1]} (Index: {element[2] // hparams['num_repeats'] + 1})')
 
 while True:
 
