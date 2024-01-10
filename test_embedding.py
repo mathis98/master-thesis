afbfd1389917154fc6cf786caf30d2b2a6e4f957
Mathis Arend <mathisarend@Mathiss-MacBook-Pro.local>
2024-01-10 16:03:04 +0800

    too slow :\

--- test_embedding.py
+++ test_embedding.py
@@ -137,12 +137,9 @@ image_embeddings, labels = full_pipeline.calculate_embeddings_for_images(validat
 
 random_sample = random.sample(list(text_data_module.test_dataset), 5)
 
-all_images = list(image_data_module.dataset)
-
 print('5 Random samples:')
 for element in random_sample:
 	print(f'Sentence: {element[1]} (Index: {element[2] // hparams["num_repeats"] + 1})')
-	print(all_images[element[2]])
 
 while True:
 
