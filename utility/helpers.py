8bf3bfd4839518df676ff07b9e702e5e4830eb0e
Mathis Arend <mathisarend@Mathiss-MacBook-Pro.local>
2024-01-30 11:54:09 +0800

    push to cuda

--- utility/helpers.py
+++ utility/helpers.py
@@ -173,7 +173,7 @@ def calculate_mAP(image_embeddings, caption_embeddings, ground_truth_labels, top
 			image_scores = torch.tensor(np.mean(image_scores_list, axis=0))
 
 			# calculate mAP and recall based on this mean
-			relevant_labels = ground_truth_labels[idx]
+			relevant_labels = torch.tensor(ground_truth_labels[idx])
 
 			print('image_scores:')
 			print(image_scores)
