3c79cebbed47dc2da305290b3e427976943415b0
Mathis Arend <mathisarend@Mathiss-MacBook-Pro.local>
2024-02-06 15:55:01 +0800

    debug output

--- test_embedding.py
+++ test_embedding.py
@@ -159,6 +159,8 @@ random_sample = random.sample(list(text_data_module.test_dataset), 5)
 print('5 Random samples:')
 for element in random_sample:
 
+	print(element)
+
 	name = ''
 	index = element[2] // hparams["num_repeats"]
 
