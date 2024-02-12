d07183a4749ebaf38687080b726d0b4a4e237ad7
Mathis Arend <mathisarend@Mathiss-MacBook-Pro.local>
2024-02-12 11:30:47 +0800

    change np mean dim

--- model/full_pipeline.py
+++ model/full_pipeline.py
@@ -445,11 +445,11 @@ class FullPipeline(pl.LightningModule):
 				print(bert_emb_list)
 
 				bert_emb_list = self.fc_layer(bert_emb_list)
-				bert_emb_list = bert_emb_list.squeeze(0)
+				bert_emb_list = bert_emb_list.squeeze()
 
 			print(f'bert_emb_list: {bert_emb_list}')
 
-			caption_embed = torch.mean(bert_emb_list, dim=-1)
+			caption_embed = torch.mean(bert_emb_list, dim=2)
 
 			print(f'caption embed: {caption_embed}')
 
