b4e1f56e5fd9edd74135fd18e55628ca1c804436
Mathis Arend <mathisarend@Mathiss-MacBook-Pro.local>
2024-02-19 19:33:38 +0800

    fix

--- model/full_pipeline.py
+++ model/full_pipeline.py
@@ -261,8 +261,8 @@ class FullPipeline(pl.LightningModule):
 				# Add to list
 				bert_emb_list.append(bert_embed)
 
-			sep = torch.tensor([self.tokenizer.sep_token_id], device='cuda:3')
-			concatenated_embeddings = torch.cat(bert_emb_list[:-1] + [sep] + [bert_emb_list[-1]], dim=0)
+			sep = torch.full_like(bert_emb_list[0][:1], tokenizer.sep_token_id, device='cuda:3')
+			concatenated_embeddings = torch.cat([emb for sublist in zip(bert_emb_list[:-1], [sep] * (len(bert_emb_list) - 1)) for emb in sublist] + [bert_emb_list[-1]], dim=0)
 
 			print(f'bert emb list: {bert_emb_list}')
 			print(f'concatenated embeddigns: {concatenated_embeddings}')
